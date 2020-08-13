485e96d63ef31180f4cad8f034cec0ff4ed006b2
Matheus Alves <matheusalves.aa@outlook.com>
2020-08-13 11:41:24 -0300

    alterações em comando.js e reupdate da pasta que estva errada

--- exercicio17-parte1/comando.js
+++ exercicio17-parte1/comando.js
@@ -1,12 +1,16 @@
 function capturar(){
     var capturando = document.getElementById('val_entrada').value
-    
-    for(linha = 0; linha < 10; linha = linha + 1){
-        var saida = document.getElementById('caixa_text')   
-        var novo_elemento = document.createElement("option")
-        var texto = document.createTextNode(' ')
-        novo_elemento.appendChild(texto)
-        saida.appendChild(novo_elemento)
-        
-    }
+    var texto = document.createTextNode(`Valor ${capturando} foi adicionado!`)
+    var saida = document.getElementById('caixa_text')   
+    var novo_elemento = document.createElement("option")
+    novo_elemento.appendChild(texto)
+    saida.appendChild(novo_elemento)
+}
+
+function finalizar(){
+    var saida = document.body
+    var texto = document.createTextNode(`testando`)
+    var novo_elemento = document.createElement("option")
+    novo_elemento.appendChild(texto)
+    saida.appendChild(novo_elemento)
 }
